a0f14d79ad747ffb5c69cbe4df83228d30317b1f
alangeorge123 <96255272+alangeorge123@users.noreply.github.com>
2021-12-17 09:09:42 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+
+<!doctype html><html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="mystyle.css">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+		<link rel="stylesheet" href="mystyle.css">
+	</head>
+	<body>
+		<div class="container d-flex justify-content-center align-items-center">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="">
+						<h3 class="pb-5">Shorten URL</h3>
+						<div>
+							<button  type="button" class="btn btn-primary btn-lg mb-4" onClick="window.location.href='save_url.php';">  
+								Shorten URL  
+							</button>
+						</div>
+						<div>
+							<button type="button" class="btn btn-primary btn-lg" onClick="window.location.href='length_url.php';">  
+								Lengthy URL  
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	</body>
+	</html>
+
+
+
+
